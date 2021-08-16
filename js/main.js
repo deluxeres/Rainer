9f009f80e9b0695c7f8d68b19a3e161f4deec9a0
Ruslan <rstebvskiy123@gmail.com>
2021-08-16 21:39:40 +0300

    Rainer 1.0

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$('#how-start').click(function () {
+    $('html, body').animate({scrollTop: $('#steps').offset().top});
+});
+
+$('#servers').click(function () {
+    $('html, body').animate({scrollTop: $('#monitoring').offset().top});
+});
